e56b3e15849f15e488dd50392ba0bdbe8fcfe8e9
Maicol <pirlo1121s@gmail.com>
2025-08-27 10:52:44 -0500

    example.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,31 +1,21 @@
-import { createProduct } from './products.js'
-import { createUser } from './users.js'
-import { calcularPrecio, descuento } from './carrito.js'
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+let impares = [];
+let pares = [];
+let sumaPares = 0;
+let sumaImpares = 0;
 
-let user1 = createUser("juan" , "juan@gmail.com")
-let user2 = createUser("maria", "maria@gmail.com")
+numeros.forEach((num)=>{
+    if(num % 2 === 0){
+        sumaPares += num
+        pares.push( num )
+    } else {
+        sumaImpares += num
+        impares.push( num )
+    }
+})
 
-let product1 = createProduct("zapatos", 500)
-let product2 = createProduct("gorra", 100)
-let product3 = createProduct("camara", 1500)
-let product4 = createProduct("audifonos", 900)
-let product5 = createProduct("pc", 2500)
+console.log(`la suma de: ${pares} es = ${sumaPares}`)
+console.log(`la suma de: ${impares} es = ${sumaImpares}`)
 
-let carrito = [product1, product2, product3, product4, product5];
 
-let precioTotal = calcularPrecio(carrito)
-let precioDescuento = descuento(precioTotal, 20);
 
-// console.log('precio total: ' + precioTotal)
-// console.log('precio con descuento: ' + precioDescuento)
-
-let mercado1 = createProduct("arroz", 20)
-let mercado2 = createProduct("leche", 30)
-let mercado3 = createProduct("pasta", 40)
-
-let carritoMercado = [mercado1,mercado2,mercado3]
-let precioMercado = calcularPrecio(carritoMercado)
-let descuentoMercado = descuento(precioMercado, 50)
-
-console.log('precio total del mercado: ' + precioMercado)
-console.log('precio con descuento del mercado: ' + descuentoMercado)
